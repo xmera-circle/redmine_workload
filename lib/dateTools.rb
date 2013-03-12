b9da16e7209893b6ff1404aa4968e6962fd6d6e9
Adrien Birbaumer <abirbaumer@gmail.com>
2013-03-12 18:06:13 +0100

    small step holidays 2

--- lib/dateTools.rb
+++ lib/dateTools.rb
@@ -4,6 +4,7 @@ class DateTools
 	
  def init_holidays()
 	@holidays[DateTime.new(2013,1,1)] = true
+	@holidays[DateTime.new(2013,3,29)] = true
 	#DateTime.new(2013,1,1),
 	#DateTime.new(2013,1,2),
 	#DateTime.new(2013,3,29),
@@ -40,7 +41,7 @@ class DateTools
     end
 
     while (inicio.to_time <= fin.to_time ) do
-      if inicio.cwday < 6 then
+      if (inicio.cwday < 6 && !@holidays.include?(inicio) )then
           days = days + 1
       end
       
@@ -53,7 +54,7 @@ end
     fecha = fecha.to_date if fecha.respond_to?(:to_date)
     while days > 0
       fecha = fecha.next
-      if fecha.cwday < 6 then
+      if (fecha.cwday < 6 && !@holidays.include?(fecha) ) then
         days = days - 1
       end
       
