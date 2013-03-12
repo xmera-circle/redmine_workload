4e2f3a403c6cdd09f2c9224ae95bead54ba81e84
Adrien Birbaumer <abirbaumer@gmail.com>
2013-03-12 17:40:05 +0100

    still debuging

--- lib/dateTools.rb
+++ lib/dateTools.rb
@@ -1,10 +1,10 @@
 class DateTools
-
+/*
   @holidays = {}
 	
   def init_holidays()
 	@holidays[DateTime.new(2013,1,1)] = true
-	/*	DateTime.new(2013,1,1),
+	DateTime.new(2013,1,1),
 	DateTime.new(2013,1,2),
 	DateTime.new(2013,3,29),
 	DateTime.new(2013,4,1),
@@ -12,9 +12,9 @@ class DateTools
 	DateTime.new(2013,5,20),
 	DateTime.new(2013,8,1),
 	DateTime.new(2013,9,16),
-	DateTime.new(2013,12,25)*/
+	DateTime.new(2013,12,25)
   end
-
+*/
   def distance_of_time_in_days(from_time, to_time = 0, inclusive = true)
     from_time = from_time.to_time if from_time.respond_to?(:to_time)
     if inclusive then
@@ -40,7 +40,7 @@ class DateTools
     end
 
     while (inicio.to_time <= fin.to_time ) do
-      if inicio.cwday < 6 && !@holidays.include?(inicio) then
+      if inicio.cwday < 6 then
           days = days + 1
       end
       
@@ -53,7 +53,7 @@ end
     fecha = fecha.to_date if fecha.respond_to?(:to_date)
     while days > 0
       fecha = fecha.next
-      if fecha.cwday < 6 && !@holidays.include?(fecha) then
+      if fecha.cwday < 6 then
         days = days - 1
       end
       
