7c896d89bfe627ed7518fe62308ae4b62832be86
Jost Baron <j.baron@netzkoenig.de>
2013-09-27 10:50:56 +0200

    [Bugfix] Added default settings for the plugin.

--- init.rb
+++ init.rb
@@ -16,7 +16,19 @@ Redmine::Plugin.register :redmine_workload do
   menu :top_menu, :WorkLoad, { :controller => 'work_load', :action => 'show' }, :caption => :workload_title,
     :if =>  Proc.new { User.current.logged? }
 
-  settings :default => {'empty' => true}, :partial => 'settings/workload_settings'
+  settings :partial => 'settings/workload_settings',
+           :default => {
+              "general_workday_monday"    => 'checked',
+              "general_workday_tuesday"   => 'checked',
+              'general_workday_wednesday' => 'checked',
+              'general_workday_thursday'  => 'checked',
+              'general_workday_friday'    => 'checked',
+              'general_workday_saturday'  => '',
+              'general_workday_sunday'    => '',
+              'threshold_lowload_min'     => 0.1,
+              'threshold_normalload_min'  => 7,
+              'threshold_highload_min'    => 8.5
+           }
 
   permission :view_project_workload, :work_load => :show
 
