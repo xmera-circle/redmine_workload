16fcb2ebae461c6bab12e753cd742b43e57b7f56
Liane Hampe <liaham@xmera.de>
2022-03-09 12:46:14 +0100

    Deprecates WorkLoadHelper#workload_admin?
    The methods is unused and will be removed in Redmine Workload 2.0.0.

--- app/helpers/work_load_helper.rb
+++ app/helpers/work_load_helper.rb
@@ -1,10 +1,18 @@
 # frozen_string_literal: true
 
 module WorkLoadHelper
-  def is_workload_admin
+  ##
+  # Unused method in this plugin. Moreover there seems to be no 'allowed_users'
+  # setting.
+  #
+  # @deprecated Will be removed from Redmine Workload 2.0.0
+  #
+  def workload_admin?
     allowed_users = Setting.plugin_redmine_workload['allowed_users']
     allowed_users.present? && allowed_users.include?(User.current.id.to_s)
   end
+
+  deprecate :workload_admin?, deprecator: RedmineWorkload.major_release_deprecator
 
   def render_action_links
     links = []
@@ -12,7 +20,7 @@ module WorkLoadHelper
     links << link_to(l(:workload_title), controller: 'work_load', action: 'show')
 
     # if is_workload_admin
-    links << link_to(l(:workload_holiday_title), controller: 'wl_national_holiday', action: 'index')
+    links << link_to(l(:workload_holiday_title), controller: 'wl_national_holiday', action: 'index') if workload_admin?
     # end
 
     # if User.current.allowed_to_globally?(:edit_user_data)
--- lib/redmine_workload.rb
+++ lib/redmine_workload.rb
@@ -2,3 +2,9 @@
 
 require 'redmine_workload/date_tools'
 require 'redmine_workload/list_user'
+
+module RedmineWorkload
+  def self.major_release_deprecator
+    ActiveSupport::Deprecation.new('2.0.0', 'Redmine Workload')
+  end
+end
