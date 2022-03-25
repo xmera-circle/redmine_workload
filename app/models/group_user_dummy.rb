b8ab9bfb8f9d44c69e58e6f3c28ef025fe40e5f8
Liane Hampe <liaham@xmera.de>
2022-03-25 16:14:37 +0100

    Calculates group workload thresholds
    The calculation will be done by the GroupUserDummy responding to
    the same threshold attributes as the WlUserData model.

---
+++ app/models/group_user_dummy.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require 'forwardable'
+
+##
+# Dummy representing a user of a group who holds all issues haven't been assigned
+# to a real group member yet.
+#
+class GroupUserDummy
+  include Redmine::I18n
+  extend Forwardable
+
+  def_delegators :group, :id, :firstname, :users
+
+  attr_reader :group
+
+  ##
+  # @params group [Group] An instance of Group model.
+  #
+  def initialize(group:)
+    self.group = group
+  end
+
+  def lastname
+    l(:label_assigned_to_group, value: group.lastname)
+  end
+
+  def threshold_lowload_min
+    sum_up(:threshold_lowload_min)
+  end
+
+  def threshold_normalload_min
+    sum_up(:threshold_normalload_min)
+  end
+
+  def threshold_highload_min
+    sum_up(:threshold_highload_min)
+  end
+
+  private
+
+  attr_writer :group
+
+  def sum_up(attribute)
+    return 0.0 unless group_members.presence
+
+    group_members.sum(&attribute.to_sym)
+  end
+
+  def group_members
+    WlUserData.where(user_id: group_member_ids, main_group: id)
+  end
+
+  def group_member_ids
+    users.map(&:id)
+  end
+end
--- lib/redmine_workload/wl_user_data_finder.rb
+++ lib/redmine_workload/wl_user_data_finder.rb
@@ -8,8 +8,9 @@ module WlUserDataFinder
   # Finds the workload data of the current user or creates them if not found.
   # When @user_workload_data is a new record default values will be assigned.
   #
-  def find_user_workload_data
-    @user_workload_data = WlUserData.find_or_create_by(user_id: User.current.id)
+  def find_user_workload_data(user_id = User.current.id)
+    @user_workload_data = WlUserData.find_or_create_by(user_id: user_id)
     @user_workload_data.update_to_defaults_when_new
+    @user_workload_data
   end
 end
