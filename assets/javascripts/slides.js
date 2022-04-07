ee311de40b618573c2331893ed2d81d7bc7a49bf
Liane Hampe <liaham@xmera.de>
2022-04-07 17:52:19 +0200

    Fixes inconsistent hierarchy tree collapsing
    When collapsing on a hierarchy level where are still open elements in
    a lower level they won't be uncollapsed anymore. That is, all elements
    below a clicked element will collapse.

--- assets/javascripts/slides.js
+++ assets/javascripts/slides.js
@@ -1,16 +1,49 @@
+/**
+	Collapse the hierarchie tree independently of the hierarchie level where
+	the collapsing will be triggered.
+ */
+
 $(document).ready(function() {
 	$('.trigger').click(function() {
+		var OPEN = '&#x25bc;'
+		var CLOSED = '&#x25b6;'
 		$(this).toggleClass('closed open');
 		affectedObjectsClass = $(this).attr('data-for');
-		
+		hierarchieTree = new Map([
+			["group-description", "user-total-workload-in-group"],
+			["user-description", "project-total-workload"],
+			["project-description", "issue-workloads"]
+		]);
+		currentLevel = $(this).parent().attr('class');		
 		if ($(this).hasClass('open')) {
-			$('.' + affectedObjectsClass + '-open').show();
-			$(this).html('&#x25bc;');
+			$(this).show();
+			$(this).siblings().show();
+			nextLevel = hierarchieTree.get(currentLevel);
+			nextLevelClass = '.' + nextLevel;
+			$(nextLevelClass).each(function(){
+				if ($(this).hasClass(affectedObjectsClass + '-open')) {
+					$(this).show();
+				}
+			});
+			$(this).html(OPEN);
 		}
 		else {
-			$('.' + affectedObjectsClass + '-close').hide();
-			$(this).html('&#x25b6;');
-			$affectedObjects.find('.trigger').html('&#x25b6;');
+			if (currentLevel == 'group-description') {
+				hierarchieTree.forEach(function(value, key){
+					nextLevelClass = '.' + value;
+					$(nextLevelClass).hide();
+					current = $(nextLevelClass).find('span.trigger.open').html(CLOSED);
+					current.siblings('dl').hide();
+				})
+				$(this).siblings().hide();
+			}
+			else {
+				$('.' + affectedObjectsClass + '-close').each(function(){
+					$(this).hide();
+					$(this).find('span').html(CLOSED);
+				})
+			}
+			$(this).html(CLOSED);
 		}
 	});
 });
