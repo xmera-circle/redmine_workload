dc02ac37216ba9dcc5328d8c68877e5467d2a57c
Liane Hampe <liaham@xmera.de>
2022-05-08 13:25:49 +0200

    Modifies workload_object_helper.rb
    RedmineWorkload::WorkloadObjectHelper module is extended with some
    switches for vacation and main group settings in order to make the
    module usable for very different test settings.

--- test/workload_object_helper.rb
+++ test/workload_object_helper.rb
@@ -2,7 +2,21 @@
 
 module RedmineWorkload
   module WorkloadObjectHelper
-    def prepare_group_workload(user:, role:, groups: nil)
+    ##
+    # Creates all objects required to analyse user or group workload objects
+    # in tests.
+    #
+    # @param user [User] The current user.
+    # @param role [Role] The role of the group to which issues should be assigned to.
+    # @param main_group_strategy [Symbol|String] The strategy (:same or :distinct)
+    #        to be used for setting the main group for user1 and user2.
+    # @param vacation_strategy [Symbol|String] The strategy (:same or :distinct)
+    #        to be used for setting up vacation for user1 and user2.
+    # @param groups [Array(Group)] A list of groups, e.g., given by the method
+    #        RedmineWorkload::WorkloadObjectHelper#groups_defined.
+    # @return [GroupWorkload] A GroupWorkload object.
+    #
+    def prepare_group_workload(user:, role:, main_group_strategy:, vacation_strategy:, groups: nil)
       if groups
         groups.compact!
         groups_params = groups.map(&:id)
@@ -16,7 +30,7 @@ module RedmineWorkload
       else
         users_params = nil
       end
-      user_setup(groups: groups, users: users) if groups
+      user_setup(groups: groups, users: users, main_group_strategy: main_group_strategy, vacation_strategy: vacation_strategy) if groups
       issue_setup(groups: groups, users: users, role: role)
       group_selection = WlGroupSelection.new(groups: groups_params,
                                              user: user)
@@ -44,9 +58,18 @@ module RedmineWorkload
     def user_setup(**params)
       group1, group2 = params[:groups]
       user1, user2 = params[:users]
+      vacation_strategy = params[:vacation_strategy]
+      main_group_strategy = params[:main_group_strategy]
+
+      if vacation_strategy
+        user1.wl_user_vacations.create(date_from: first_day, date_to: first_day)
+        user2.wl_user_vacations.create(with_vacation_strategy(vacation_strategy))
+      end
 
       user1.groups << group1
       assert user1.groups.include? group1
+      user1.groups.reload
+      assert Group.find(group1.id).users.include? user1
 
       user1.create_wl_user_data(threshold_highload_min: 6,
                                 threshold_lowload_min: 3,
@@ -54,15 +77,37 @@ module RedmineWorkload
                                 main_group: group1.id)
       assert_equal group1.id, user1.wl_user_data.main_group
 
-      user2.groups << [group1, group2]
+      user2.groups << group1
+      user2.groups << group2
       assert user2.groups.include? group1
       assert user2.groups.include? group2
+      user2.groups.reload
+      assert Group.find(group1.id).users.include? user2
+      assert Group.find(group2.id).users.include? user2
 
       user2.create_wl_user_data(threshold_highload_min: 6,
                                 threshold_lowload_min: 3,
                                 threshold_normalload_min: 4,
-                                main_group: group2.id)
-      assert_equal group2.id, user2.wl_user_data.main_group
+                                main_group: with_main_group_strategy(group1, group2, main_group_strategy))
+      assert_equal with_main_group_strategy(group1, group2, main_group_strategy), user2.wl_user_data.main_group
+    end
+
+    def with_main_group_strategy(group1, group2, main_group_strategy)
+      case main_group_strategy.to_sym
+      when :distinct
+        group2.id
+      when :same
+        group1.id
+      end
+    end
+
+    def with_vacation_strategy(vacation_strategy)
+      case vacation_strategy.to_sym
+      when :distinct
+        { date_from: last_day, date_to: last_day }
+      when :same
+        { date_from: first_day, date_to: first_day }
+      end
     end
 
     def issue_setup(**params)
@@ -100,11 +145,11 @@ module RedmineWorkload
     end
 
     def first_day
-      Date.new(2013, 5, 25)
+      Date.new(2022, 5, 25) # Wednesday
     end
 
     def last_day
-      Date.new(2013, 6, 4)
+      Date.new(2022, 5, 30) # Monday
     end
   end
 end
